45b275e2e11da3e3b35213b9d0f76bbb9eca3582
TF <tahreemfatima663@gmail.com>
2025-04-25 15:42:51 +0500

    friday changings

--- routes/addMovie.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-router.get("/fetch-movie", async (req, res) => {
-  console.log("movie fetched");
-});
-module.exports = router;
